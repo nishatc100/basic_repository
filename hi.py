7120f5caa48a9a5e27d190256039fd503d93b245
Nishat Chowdhury <nishtc@umd.edu>
2023-04-02 21:18:23 -0400

    created dictionary of tutorial instructions

--- hi.py
+++ hi.py
@@ -1,1 +1,7 @@
 print("hi")
+snapshot = {
+    "stage": "git add (filename.py)",
+    "commit": "git commit -m 'snapshot message'", 
+    "commit message example": "git commit -m created dictionary of instructions for snapshotting work "
+
+}
