5abf21ef3c93c483da73ddad9d2b2b73d266b93c
Nishat Chowdhury <nishtc@umd.edu>
2023-04-02 21:03:24 -0400

    added basic_repo.py

---
+++ basic_repo.py
@@ -0,0 +1,9 @@
+'''A file to follow the git tutorial'''
+
+class Person:
+    '''A person
+    
+    Attributes:
+        name (str): person's name
+        connections (set of Person): other people the person knows
+        '''
