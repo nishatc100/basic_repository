d6a379ea0d5ac95dd81a29974d2b40d5c1d3a63f
Nishat Chowdhury <nishtc@umd.edu>
2023-04-02 21:24:40 -0400

    add merge conflict dictionary

--- hi.py
+++ hi.py
@@ -5,3 +5,11 @@ snapshot = {
     "commit message example": "git commit -m created dictionary of instructions for snapshotting work "
 
 }
+
+#snapshot pushes changes to collaborators
+
+
+merge_conflict = {
+    "what is it?": "when a collaborators work conflicts with your own",
+    "solution?": "accept one's work or the other, then add file again by snapshotting it"
+}
